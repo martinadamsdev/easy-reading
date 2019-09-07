b3f56b90aff179ff495d2b8b048bb31fec0792ba
w3cfed <w3cfed@gmail.com>
2019-09-08 00:08:25 +0800

    添加标题栏点击切换事件

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="ebook">
+    <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+      <div class="left">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-person icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-more icon"></span>
+        </div>
+      </div>
+    </div>
+    <div id="read"></div>
     <div class="read-wrapper">
-      <div id="read"></div>
-      <div class="mask"></div>
-        <div class="left"></div> 
-        <div class="center"></div>
-        <div class="right"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center" @click="toggleTitleAndMenu">
+
+        </div>
+        <div class="right" @click="nextPage"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +34,25 @@
   const DOWNLOAD_URL = '/static/test.epub'
   export default {
     name: "Ebook",
+    data () {
+      return {
+        ifTitleAndMenuShow: false
+      }
+    },
     methods: {
+      toggleTitleAndMenu () {
+        this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      },
+      prevPage () {
+        if (this.redetion) {
+          this.redetion.prev()
+        }
+      },
+      nextPage () {
+        if (this.redetion) {
+          this.redetion.next()
+        }
+      },
       // 电子书的解析和渲染
       showEpub() {
         // 生成 Ebook
@@ -36,10 +73,28 @@
   }
 </script>
 
-<style scoped lang="sass">
+<style lang="sass">
   @import "assets/styles/global"
   .ebook
     position: relative
+    .title-wrapper
+      position: absolute
+      top: 0
+      left: 0
+      z-index: 101
+      display: flex
+      width: 100%
+      height: px2rem(48)
+      background: white
+      box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15)
+      .left
+        flex: 0 0 px2rem(60)
+      .right
+        flex: 1
+        display: flex
+        justify-content: flex-end
+        .icon-wrapper
+          flex: 0 0 px2rem(40)
     .read-wrapper
       .mask
         position: absolute
@@ -51,14 +106,14 @@
         height: 100%
         .left
           height: 100%
-          flex: 0 0 pe2rem(100)
-          background: yellow 
+          flex: 0 0 px2rem(100)
+          /*background: yellow */
         .center
           height: 100%
           flex: 1
-          background: red
+          /*background: red*/
         .right
           height: 100%
-          flex: 0 0 ps2rem(100)
-          background: orange   
+          flex: 0 0 px2rem(100)
+          /*background: orange   */
 </style>
