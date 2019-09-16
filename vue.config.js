739f85803efe44480cc932f86cccbe43f649e4c9
w3cfed <w3cfed@gmail.com>
2019-09-16 22:00:55 +0800

    项目开发版初始化

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+'use strict'
+const path = require('path')
+const resolve = dir => {return path.join(__dirname, dir)}
+
+module.exports = {
+  // assetsDir: './src/static',
+  // lintOnSave: true, // 是否开启eslint保存检测
+  // productionSourceMap: false, // 是否在构建生产包时生成sourcdeMap
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('views', resolve('src/views'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'));
+    // config.optimization.runtimeChunk('single')
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: '8080',
+    hot: true,
+    open: true,
+    overlay: {
+      warning: false,
+      error: true
+    }
+  }
+}
